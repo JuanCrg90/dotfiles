eecba8f33b6228fd4cfe86d19d6a6695c8e36a3a
Juan Carlos Ruiz <JuanCrg90@gmail.com>
2015-12-10 19:58:03 -0600

    added zsh profile

--- zsh/zsh-install.sh
+++ zsh/zsh-install.sh
@@ -4,4 +4,8 @@
 echo -e "create symlink for .zshrc"
 ln -sf $(pwd)/zshrc.symlink ~/.zshrc
 
+# create symlink for .gitconfig
+echo -e "create symlink for .zprofile"
+ln -sf $(pwd)/zprofile.symlink ~/.zprofile
+
 
