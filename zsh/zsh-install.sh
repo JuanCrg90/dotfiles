1e0baa14adc611e87883a2ab7fcd49b4cda11dc9
Juan Carlos Ruiz <JuanCrg90@gmail.com>
2016-02-17 19:29:15 -0600

    Updated ZSH config

--- zsh/zsh-install.sh
+++ zsh/zsh-install.sh
@@ -1,11 +1,6 @@
 #!/usr/bin/sh
 
-# create symlink for .gitconfig
+# create symlink for .zshrc
 echo -e "create symlink for .zshrc"
 ln -sf $(pwd)/zshrc.symlink ~/.zshrc
-
-# create symlink for .gitconfig
-echo -e "create symlink for .zprofile"
-ln -sf $(pwd)/zprofile.symlink ~/.zprofile
-
 
