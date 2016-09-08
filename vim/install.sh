218763d74bc1bb4eca4c7e5c521071b33ffbb30d
Juan Carlos Ruiz <JuanCrg90@gmail.com>
2016-09-08 00:21:28 -0500

    Improve the vim instalation

---
+++ vim/install.sh
@@ -0,0 +1,21 @@
+#!/user/bin/sh
+
+RED='\033[0;31m'
+NC='\033[0m' # No Color]]'
+
+echo "[*] Install vim"
+brew install vim --with-python --with-ruby --with-perl
+
+echo "[*] Create symlink for .vimrc"
+ln -sf $(pwd)/vimrc.symlink ~/.vimrc
+
+# create a symlink for the vim directory
+echo "[*] Create symlink for .vim directory"
+ln -sf $(pwd)/vim.symlink ~/.vim
+
+echo "[*] Download Vundle"
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+echo "[*] Done. please enter to vim and run ${RED}:PluginInstall${NC}"
+
+
