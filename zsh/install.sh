21e2a8fdff436acf00716348c29d33a18ae79c6a
Juan Carlos Ruiz <JuanCrg90@gmail.com>
2017-01-15 00:27:00 -0600

    Update Zsh Instalation script

--- zsh/install.sh
+++ zsh/install.sh
@@ -9,6 +9,10 @@ sudo -s "echo /usr/local/bin/zsh >> /etc/shells" && chsh -s /usr/local/bin/zsh
 echo "Install Oh-My-Zsh"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
+
+echo "Install powerlevel9k Theme"
+git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
+
 echo "create symlink for .zshrc"
 ln -sf $(pwd)/zshrc.symlink ~/.zshrc
 
