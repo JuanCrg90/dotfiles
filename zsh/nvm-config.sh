eec2d0cb8a769400a121d0ff8796e81fe29723fb
Juan C. Ruiz <JuanCrg90@gmail.com>
2025-01-25 19:45:31 -0600

    refactor(zsh/nvm-config.sh): Update NVM path and ensure Homebrew installation is checked
    - Updated NVM_DIR to use Homebrew-installed paths
    - Added checks for both /usr/local/opt and /opt/homebrew/opt directories
    to handle different installations

--- zsh/nvm-config.sh
+++ zsh/nvm-config.sh
@@ -1,6 +1,6 @@
 export NVM_DIR="$HOME/.nvm"
-[ -s "/usr/local/opt/nvm/nvm.sh" ] && . "/usr/local/opt/nvm/nvm.sh"  # This loads nvm
-[ -s "/usr/local/opt/nvm/etc/bash_completion.d/nvm" ] && . "/usr/local/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
+[ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && \. "/opt/homebrew/opt/nvm/nvm.sh"  # This loads nvm
+[ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
 
 # place this after nvm initialization!
 autoload -U add-zsh-hook
