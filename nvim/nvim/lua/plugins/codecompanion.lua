f22940908285adde7ac842ce0bd92a89de24b9d2
Juan C. Ruiz <juan.ruiz@generalassemb.ly>
2025-07-22 11:44:03 -0600

    Add codecompanion ollama configuration

--- nvim/nvim/lua/plugins/codecompanion.lua
+++ nvim/nvim/lua/plugins/codecompanion.lua
@@ -6,17 +6,20 @@ return {
       "nvim-treesitter/nvim-treesitter",
     },
     opts = function()
-      local adapter = "openai" -- Default adapter
+      local adapter = "ollama" -- Default adapter
+      local model = "gpt-4.1"
 
       -- Detect if the system supports Ollama (e.g., M1/M2 Mac)
       if vim.fn.has("macunix") == 1 and vim.loop.os_uname().machine == "arm64" then
         adapter = "ollama"
+        model = "qwen2.5-coder:latest"
       end
 
       return {
         strategies = {
           chat = {
             adapter = adapter,
+            model = model,
             roles = { llm = "Morpheus", user = "JuanCrg90" },
             slash_commands = {
               ["buffer"] = {
@@ -47,8 +50,44 @@ return {
         adapters = {
           openai = function()
             return require("codecompanion.adapters").extend("openai", {
+              schema = {
+                model = {
+                  default = model,
+                },
+              },
               env = {
                 api_key = vim.fn.getenv("OPENAI_API_KEY"),
+              },
+            })
+          end,
+          ollama = function()
+            return require("codecompanion.adapters").extend("ollama", {
+              name = adapter, -- Give this adapter a different name to differentiate it from the default ollama adapter
+              opts = {
+                vision = true,
+                stream = true,
+              },
+              schema = {
+                model = {
+                  default = model,
+                },
+                num_ctx = {
+                  --default = 16384,
+                },
+                think = {
+                  -- default = false,
+                  -- or, if you want to automatically turn on `think` for certain models:
+                  default = function(adapter)
+                    -- this'll set `think` to true if the model name contain `qwen3` or `deepseek-r1`
+                    local model_name = adapter.model.name:lower()
+                    return vim.iter({ "qwen3", "deepseek-r1" }):any(function(kw)
+                      return string.find(model_name, kw) ~= nil
+                    end)
+                  end,
+                },
+                keep_alive = {
+                  default = "5m",
+                },
               },
             })
           end,
