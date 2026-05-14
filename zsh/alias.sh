5d466bb5c61ff7104a045865d50bcc760e9afc1a
Juan C. Ruiz <juancrg90@192.168.4.34>
2026-05-14 11:49:59 -0600

    Remove legacy alias

--- zsh/alias.sh
+++ zsh/alias.sh
@@ -1,13 +1,9 @@
 alias zshconfig="vim ~/dotfiles/zsh"
 alias la="ls -al"
 alias :q="exit"
-alias speedtest="docker run --rm --net=host tianon/speedtest"
 alias bubc='brew upgrade && brew cleanup'
 alias gapan='git add --intent-to-add . && git add --patch'
 
-alias apr='sudo apachectl restart'
-alias cdpm='cd ~/easybroker/property_manager'
-alias mysqlpm='mysql -u some_new_user property_manager_development -p'
 alias be='bundle exec'
 alias ber='bundle exec rails'
 alias bert='bundle exec rails test'
