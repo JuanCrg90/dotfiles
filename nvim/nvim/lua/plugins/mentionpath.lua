12b2c10cd4250c86ffbcd80f6dbccfe02e98659d
Juan C. Ruiz <JuanCrg90@gmail.com>
2026-09-20 23:14:10 -0600

    my mentionpath plugin

---
+++ nvim/nvim/lua/plugins/mentionpath.lua
@@ -0,0 +1,27 @@
+return {
+  {
+    "mentionpath.nvim",
+    dir = "~/Projects/mentionpath.nvim",
+    ft = "markdown",
+    dependencies = { "saghen/blink.cmp" },
+    opts = {
+      ui = {
+        backend = "blink",
+      },
+      debug = {
+        enabled = true,
+      },
+    },
+  },
+  {
+    "saghen/blink.cmp",
+    opts = function(_, opts)
+      opts.sources = opts.sources or {}
+      opts.sources.default = opts.sources.default or { "lsp", "path", "snippets", "buffer" }
+      table.insert(opts.sources.default, 1, "mentionpath")
+
+      opts.sources.providers = opts.sources.providers or {}
+      opts.sources.providers.mentionpath = require("mentionpath").blink_provider()
+    end,
+  },
+}
