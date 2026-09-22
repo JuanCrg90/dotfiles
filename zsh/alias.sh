c5e36ab55b4b81144455ee4546fe21390dab5cf3
Juan C. Ruiz <JuanCrg90@gmail.com>
2026-09-22 10:20:15 -0600

    Fix committer alias

--- zsh/alias.sh
+++ zsh/alias.sh
@@ -7,14 +7,14 @@ alias be='bundle exec'
 alias ber='bundle exec rails'
 alias bert='bundle exec rails test'
 
-if [[ "$OSTYPE" == darwin* ]] && (( $+commands[brew] )); then
+if [[ "$OSTYPE" == darwin* ]] && (($ + commands[brew])); then
   alias bubc='brew upgrade && brew cleanup'
 fi
 
-if (( $+commands[nvim] )); then
+if (($ + commands[nvim])); then
   alias vim='nvim'
 fi
 
-commiter_script="$HOME/Projects/agent-scripts/scripts/commiter"
-[[ -x "$commiter_script" ]] && alias commiter="$commiter_script"
-unset commiter_script
+committer_script="$HOME/Projects/agent-scripts/scripts/committer"
+[[ -x "$committer_script" ]] && alias committer="$committer_script"
+unset committer_script
